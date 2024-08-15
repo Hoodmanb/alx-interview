64dc6c00f9c899d492f667328ea94553d0f2af03
hoodmanb <joshuadebravo@gmail.com>
2024-08-15 23:45:23 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,55 +3,60 @@ import sys
 import re
 import signal
 
+def handle(sig, frame):
+    if sig == signal.SIGINT:
+        return True
+    return False
+
+regex = r'(\d{1,3}(?:\.\d{1,3}){3}) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)'
 num = 0
 size = 0
 lists = []
-codes = [200, 301, 400, 401, 403, 404, 405, 500]
-code_counts = {code: 0 for code in codes}
-
-
-def handle(sig, frame):
-    """Handle keyboard interrupt (Ctrl+C)"""
-    if sig == signal.SIGINT:
-        return True
-    if sig != signal.SIGINT:
-        return False
-
-
-def print_summary():
-    """Print the summary of file size and code counts"""
-    print("File size: {}".format(size))
-    for code in codes:
-        if code_counts[code] > 0:
-            print("{}: {}".format(code, code_counts[code]))
-
-
-handleCall = signal.signal(signal.SIGINT, handle)
-
-regex = (
-    r'(\d{1,3}(?:\.\d{1,3}){3}) - \[(.*?)\] '
-    r'"GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)'
-)
+# a = 0
 
 while True:
     line = sys.stdin.readline()
     if not line:
         break
     line = line.strip()
-
     match = re.match(regex, line)
     if not match:
         continue
-
-    file_size = int(match.group(4))
-    size += file_size
-
-    status_code = int(match.group(3))
-    if status_code in code_counts:
-        code_counts[status_code] += 1
-
+    string = line.split()
+    size += int(string[-1])
+    lists.append(string[-2])
     num += 1
-
-    if num == 10 or handleCall:
-        print_summary()
-        num = 0
+    signals = signal.signal(signal.SIGINT, handle)
+    if num == 10 or signals:
+        print("Filse size: {}".format(size))
+        codes = [200, 301, 400, 401, 403, 404, 405, 500]
+        for c in codes:
+            sortedList = sorted(lists)
+            counts = sortedList.count(str(c))
+            if counts:
+                print('{}: {}'.format(c, counts))
+            continue
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            # for index, item in enumerate(sortedList):
+                
+                
+                
+                
+                # if item == c:
+                #     print('item:', a)
+                    # print('c: ', index)
+                #     a += 1
+                # if index == len(lists) - 1:
+                #     print(a)
+                    # print('{}: {}'.format(c, a))
+                    # print('last line: ', index)
